6f277d7241ed1b97caf7f85041444a7d7734ec2f
Jade Meskill <jade.meskill@gmail.com>
2018-04-23 09:39:14 -0700

    fix pointer to conditions

--- pkg/types/taskdefinition.go
+++ pkg/types/taskdefinition.go
@@ -2,7 +2,7 @@ package types
 
 // TaskDefinition represents a task definition
 type TaskDefinition struct {
-	Conditions *Conditions           `json:"conditions,omitempty"`
+	Conditions Conditions            `json:"conditions,omitempty"`
 	Result     *TaskDefinitionResult `json:"result"`
 	Runner     *Runner               `json:"runner"`
 }

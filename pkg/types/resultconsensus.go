40f77f91243afe8fea7d77ca9fdfc3547dcd0326
Jade Meskill <jade.meskill@gmail.com>
2018-06-11 09:30:35 -0700

    v3.2.0 add Consensus.Threshold to TaskDefinitionResult

---
+++ pkg/types/resultconsensus.go
@@ -0,0 +1,6 @@
+package types
+
+// ResultConsensus sets the consensus threshold for the results
+type ResultConsensus struct {
+	Threshold int `json:"threshold,omitempty"`
+}
--- pkg/types/taskdefinitionresult.go
+++ pkg/types/taskdefinitionresult.go
@@ -2,6 +2,7 @@ package types
 
 // TaskDefinitionResult represents a task definition result
 type TaskDefinitionResult struct {
-	Action      string       `json:"action,omitempty"`
-	Destination *DatasetLink `json:"destination"`
+	Action      string           `json:"action,omitempty"`
+	Destination *DatasetLink     `json:"destination"`
+	Consensus   *ResultConsensus `json:"consensus:omitempty"`
 }
